0ec1bd06b01c19ca3f7b77ce23b8cafcda1a5296
priya3018 <144198021+priya3018@users.noreply.github.com>
2023-09-06 10:17:39 +0530

    Add files via upload

---
+++ main challenge 2.py
@@ -0,0 +1,6 @@
+#write a program that determine whether a year is a leap year are not using if else statement
+number=int(input())
+if number%4==0:
+  print("leap year")
+else:
+  print("not a leap year")
