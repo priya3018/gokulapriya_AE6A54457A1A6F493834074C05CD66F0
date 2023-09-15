bb8e6af59f92dc6f416cda776298dd5ca1ff5185
priya3018 <144198021+priya3018@users.noreply.github.com>
2023-09-15 15:27:28 +0530

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,9 +1,13 @@
 class player:
     def play(self):
         print("The player is playing cricket.")
+
+
 class Batsman(player):
     def play(self):
         print("The batsman is bating.")
+
+
 class Bowler(player):
     def play(self):
         print("The bowler is bowling.")
